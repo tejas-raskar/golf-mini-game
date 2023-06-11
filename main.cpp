116572d0635c7a5e0331c6fabbc7784a94123f92
TREx934211 <127019430+TREx934211@users.noreply.github.com>
2023-06-11 09:50:01 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,4 +5,9 @@ int main(int argc, char* agrv[]){
   // Error
    if (SDL_Init(SDL_INIT_EVERYTHING)) 
         std::cout << SDL_GetError() <<std::endl;
+  /* Code Body */
+//     SDL_DestroyTexture(tex);
+//     SDL_DestroyRenderer(rend);
+    SDL_DestroyWindow(win);
+    SDL_Quit();
 }
