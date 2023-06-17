afb9ca4a08b2b96341756def43a9775ce0efe00b
Tejas Raskar <tjsrskr@gmail.com>
2023-06-17 19:47:26 +0530

    Initial Commit to main.cpp
    Adds a simple program that creates a window for the game.

--- main.cpp
+++ main.cpp
@@ -1,13 +1,19 @@
-#include <iostream>
-#include <SDL2/SDL.h>
+#include <raylib.h>
+#include <raymath.h>
 
-int main(int argc, char* agrv[]){
-  // Error
-   if (SDL_Init(SDL_INIT_EVERYTHING)) 
-        std::cout << SDL_GetError() <<std::endl;
-  /* Code Body */
-//     SDL_DestroyTexture(tex);
-//     SDL_DestroyRenderer(rend);
-//     SDL_DestroyWindow(win);
-    SDL_Quit();
+int main(void)
+{
+    InitWindow(400, 224, "Golf v1.0");
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+            ClearBackground(BLACK);
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
+        
 }
