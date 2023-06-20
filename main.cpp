b638501cc374a583eeff392f9ccaced834a03585
Tejas Raskar <tjsrskr@gmail.com>
2023-06-20 14:20:42 +0530

    GolfBall Physics & Basic win logic
    The golfball can now be moved with the help of the mouse with a simulation that calculates velocity based on the friction and time.

--- main.cpp
+++ main.cpp
@@ -1,19 +1,50 @@
 #include <raylib.h>
 #include <raymath.h>
+#include <iostream>
+
+#include "ball.h"
+#include "cup.h"
+
+Ball ball;
+Cup cup;
 
 int main(void)
 {
-    InitWindow(400, 224, "Golf v1.0");
+    const int screenWidth = 640;
+    const int screenHeight = 480;
+    InitWindow(screenWidth, screenHeight, "Golf v1.1");
+    SetTargetFPS(60);
+
+    cup.radius = 10;
+    cup.cupPosition.x = screenWidth / 4;
+    cup.cupPosition.y = screenHeight / 4;
+
+    ball.radius = 7;
+    ball.position.x = screenWidth / 2;
+    ball.position.y = screenHeight / 2;
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
-            ClearBackground(BLACK);
+
+        ball.update();
+
+        ClearBackground(BLACK);
+
+        DrawCircleV(cup.cupPosition, cup.radius, GRAY);
+
+        ball.draw();
+
+        if (cup.checkWin(ball.position, cup))
+        {
+            std::cout << "Congratulations! You have won!" << std:: endl;
+            break;
+        }
+
         EndDrawing();
     }
 
     CloseWindow();
 
     return 0;
-        
 }
