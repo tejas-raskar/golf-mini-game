6e716240e1d4ec80c569895f67673bded83878e7
parth10-1 <134192946+parth10-1@users.noreply.github.com>
2023-06-20 22:51:02 +0530

    Update main.cpp
    Added arrow

--- main.cpp
+++ main.cpp
@@ -1,19 +1,66 @@
 #include <raylib.h>
 #include <raymath.h>
+#include <iostream>
+#define MAX_LINE_LENGTH 200
+
+
+#include "ball.h"
+#include "cup.h"
+#include "arrow.h"
+
+
+
+
+
+Ball ball;
+Cup cup;
+Arrow arrow;
 
 int main(void)
 {
-    InitWindow(400, 224, "Golf v1.0");
+    const int screenWidth = 640;
+    const int screenHeight = 480;
+    InitWindow(screenWidth, screenHeight, "Golf v1.1");
+    SetTargetFPS(60);
 
+    cup.radius = 10;
+    cup.cupPosition.x = screenWidth / 4;
+    cup.cupPosition.y = screenHeight / 4;
+
+    ball.radius = 7;
+    ball.position.x = screenWidth / 2;
+    ball.position.y = screenHeight / 2;
+    
+    arrow.TexLoad();
+   
     while (!WindowShouldClose())
     {
+        arrow.ArrowCor(ball.position.x, ball.position.y);
+
         BeginDrawing();
-            ClearBackground(BLACK);
+
+        ClearBackground(SKYBLUE);
+        arrow.Draw();
+        
+        ball.update();
+        
+
+        DrawCircleV(cup.cupPosition, cup.radius, GRAY);
+
+        ball.draw();
+
+        if (cup.checkWin(ball.position, cup))
+        {
+            std::cout << "Congratulations! You have won!" << std:: endl;
+            break;
+        }
+
         EndDrawing();
     }
-
+   
+    arrow.TextUnload();
     CloseWindow();
 
     return 0;
-        
+
 }
